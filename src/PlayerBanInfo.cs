bdb8c4ae8ec72bbdef9ed13d865881497a3fae1c
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-11 11:34:05 +0200

    Change PlayerBanInfo into a class

--- src/PlayerBanInfo.cs
+++ src/PlayerBanInfo.cs
@@ -1,7 +1,7 @@
 ﻿namespace SharpSteamWebApi
 {
-    // Ban info container
-    public struct PlayerBanInfo
+    // Steam player ban info container.
+    public class PlayerBanInfo
     {
         public bool HasVACBan { get; set; }
         public bool HasCommunityBan { get; set; }
@@ -9,5 +9,34 @@
         public int VACBanCount { get; set; }
         public int GameBanCount { get; set; }
         public int DaysSinceLastBan { get; set; }
+
+        // Constructor for player ban info.
+        public PlayerBanInfo()
+        {
+            HasVACBan = false;
+            HasCommunityBan = false;
+            HasTradeBan = false;
+            VACBanCount = -1;
+            GameBanCount = -1;
+            DaysSinceLastBan = -1;
+        }
+
+        // Checks if this player ban info has a VAC ban count.
+        public bool HasVACBanCount()
+        {
+            return VACBanCount != -1;
+        }
+
+        // Checks if this player ban info has a game ban count.
+        public bool HasGameBanCount()
+        {
+            return GameBanCount != -1;
+        }
+
+        // Checks if this player ban info has days since last ban info.
+        public bool HasDaysSinceLastBan()
+        {
+            return DaysSinceLastBan != -1;
+        }
     }
 }
