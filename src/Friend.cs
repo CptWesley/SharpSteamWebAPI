9854f4f7a80ba02a9cb3279bad299aef521e48b5
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-12 09:01:12 +0200

    Add querying of friends

--- src/Friend.cs
+++ src/Friend.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace SharpSteamWebApi
 {
@@ -33,6 +36,57 @@ namespace SharpSteamWebApi
         public bool HasRelationship()
         {
             return Relationship != -1;
+        }
+
+        // Queries all the achieved achievement info of a player.
+        public static Friend[] Query(string apikey, long playerId)
+        {
+            string url = String.Format("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={0}&steamid={1}&format=xml", apikey, playerId);
+            XDocument xml = GetXML(url);
+
+            if (xml == null)
+                return new Friend[0];
+
+            XElement[] items = xml.Descendants("friend").ToArray();
+            Friend[] result = new Friend[items.Length];
+
+            for (int i = 0; i < items.Length; ++i)
+                result[i] = Parse(items[i]);
+
+            return result;
+        }
+
+        // Parses the xml formatted verbose owned game info to an object.
+        private static Friend Parse(XElement xml)
+        {
+            if (xml == null)
+                return null;
+
+            ElementParser parser = new ElementParser(xml);
+
+            if (!parser.GetAttributeBoolean("achieved"))
+                return null;
+
+            Friend result = new Friend
+            {
+                Id = parser.GetAttributeLong("steamid"),
+                Date = parser.GetAttributeDate("friend_since"),
+                Relationship = GetRelationshipInt(parser.GetAttributeString("relationship"))
+            };
+
+            return result;
+        }
+
+        // Get the right integer of the relationship type.
+        private static int GetRelationshipInt(string code)
+        {
+            switch (code)
+            {
+                case "friend":
+                    return SharpSteamWebApi.Relationship.Friend;
+                default:
+                    return SharpSteamWebApi.Relationship.Unknown;
+            }
         }
     }
 }
--- src/Relationship.cs
+++ src/Relationship.cs
@@ -3,6 +3,7 @@
     // Holds constants for relationship types.
     public static class Relationship
     {
+        public const int Unknown = -1;
         public const int Friend = 0;
     }
 }
