91b5aea191df3b70443f5f4523d9925045477c61
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-12 08:54:10 +0200

    Add Friend class

---
+++ src/Friend.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SharpSteamWebApi
+{
+    // Steam friend.
+    public class Friend : SSWAObject
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Relationship { get; set; }
+
+        // Constructor of a friend.
+        public Friend()
+        {
+            Id = -1;
+            Date = DateTime.MinValue;
+            Relationship = -1;
+        }
+
+        // Checks if Id info is known.
+        public bool HasId()
+        {
+            return Id != -1;
+        }
+
+        // Checks if date info is known.
+        public bool HasDate()
+        {
+            return Date != DateTime.MinValue;
+        }
+
+        // Checks if relationship info is known.
+        public bool HasRelationship()
+        {
+            return Relationship != -1;
+        }
+    }
+}
---
+++ src/Relationship.cs
@@ -0,0 +1,8 @@
+﻿namespace SharpSteamWebApi
+{
+    // Holds constants for relationship types.
+    public static class Relationship
+    {
+        public const int Friend = 0;
+    }
+}
